a57cb268469d956531eaff4e6a7effe613280e4c
Sohaash26 <96625328+Sohaash26@users.noreply.github.com>
2021-12-30 05:41:37 +0200

    ui scripts

---
+++ DYING-TO-LIVE/Assets/menus/UIMenuScripts/Navigation Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public void GoIntroScene()
+    {
+		Application.LoadLevel(0);
+    }
+	public void GoToGameScene()
+    {
+		Application.LoadLevel(1);
+    }
+	public void GoToOverScene()
+    {
+		Application.LoadLevel(2);
+
+	}
+	public void GoToVictoryScene()
+    {
+		Application.LoadLevel(3);
+	}
+	public void Quit()
+    {
+		Application.Quit();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
