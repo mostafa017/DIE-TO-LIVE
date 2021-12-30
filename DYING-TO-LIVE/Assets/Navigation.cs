ab8ad8b32a5d46e6f94a7ecf66f60931c0006e7a
Rawan221 <Rawan201683@bue.edu.eg>
2021-12-30 06:03:37 +0200

    MainMenu

--- DYING-TO-LIVE/Assets/Navigation.cs
+++ DYING-TO-LIVE/Assets/Navigation.cs
@@ -2,36 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
-
+public class Navigation : MonoBehaviour {
 	public void GoIntroScene()
-    {
+	{
 		Application.LoadLevel(0);
-    }
+	}
 	public void GoToGameScene()
-    {
+	{
 		Application.LoadLevel(1);
-    }
+	}
 	public void GoToOverScene()
-    {
+	{
 		Application.LoadLevel(2);
 
 	}
 	public void GoToVictoryScene()
-    {
+	{
 		Application.LoadLevel(3);
 	}
 	public void Quit()
-    {
+	{
 		Application.Quit();
 	}
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
 	}
 }
